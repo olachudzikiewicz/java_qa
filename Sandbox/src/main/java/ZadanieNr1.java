f2e58d2ec7214154988d1a20d581252da14c21b6
olachudzikiewicz <34106824+olachudzikiewicz@users.noreply.github.com>
2017-12-03 20:17:27 +0100

    Funkcja

--- Sandbox/src/main/java/ZadanieNr1.java
+++ Sandbox/src/main/java/ZadanieNr1.java
@@ -4,5 +4,17 @@ public class ZadanieNr1 {
 
 		System.out.println("Zadanie numer 1!");
 
+		System.out.println("Zadanie numer 2!");
+
+		Point p1 = new Point(6,2);
+		Point p2 = new Point(-3,-1);
+
+
+
+		System.out.println("Odległość między punktami wynosi: " + distance(p1,p2));
+	}
+
+	public static double distance(Point p1, Point p2) {
+		return Math.sqrt((Math.pow(p1.x - p2.x,2))+ (Math.pow(p1.y - p2.y,2)));
 	}
 }
